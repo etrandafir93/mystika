1f9308de84e87fb72b84a03da18a08d2491f754d
Emanuel Trandafir <emanueltrandafir1993@gmail.com>
2025-12-13 16:26:43 +0200

    backend app

---
+++ src/main/java/com/mystika/tarot/MistikaApplication.java
@@ -0,0 +1,13 @@
+package com.mystika.tarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MistikaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MistikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystika/tarot/MystikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystika.tarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
